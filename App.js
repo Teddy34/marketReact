8c22f6730be8e479c660b7ef3df0603b67860467
jordi <jorditeddy@gmail.com>
2015-12-01 23:39:08 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const sample = [
+	{
+		"typeID":638,
+		"typeName":"Raven",
+		"groupID":27,
+		"volume":486000,
+		"marketGroupID":80,
+		"quantity":1,
+		"mean":173815907.91,
+		"weightedMean":179480189.02,
+		"minSell":196995998.8,
+		"buyMax":185027000.01,
+		"typeId":638,
+		"volumeAvailable":0,
+		"reasonablePrice":226545398.62,
+		"_id":"56592448e5dd5d1100c97dfe",
+		"hubData":{"mean":196995998.8}
+	}
+];
+
+const iconURL ='https://image.eveonline.com/Type/';
+const iconURLSuffix = "_32.png";
+
+class Item extends React.Component {
+	render() {
+		return (<tr key={this.props.elem.typeId}>
+								<td><img src={iconURL + this.props.elem.typeId +iconURLSuffix} /></td>
+								<td>{this.props.elem.typeName}</td>
+								<td>{this.props.elem.quantity}</td>
+								<td>{this.props.elem.volumeAvailable}</td>
+								<td>{this.props.elem.hubData.mean}</td>
+								<td>{this.props.elem.reasonablePrice}</td>
+							</tr>);
+	}
+}
+
+class ItemTab extends React.Component {
+	render() {
+		return (<table>
+					<tbody>
+					{this.props.data.map( (data) => {return <Item />}}
+					</tbody>
+				</table>);
+	}
+}
+
+class App extends React.Component {
+
+	render() {
+		return <ItemTab data={this.props.data.data} />;
+	}
+
+}
+
+App.defaultProps = {
+			data: [],
+			_id: "",
+			timestamp:""
+		};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port:3333,
+    proxy: {
+      '/backend': {
+        target: 'http://localhost:8080/api/',
+        secure: false
+      }
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel-loader', // The module to load. "babel" is short for "babel-loader"
+      query: {
+        presets: ['es2015', 'react']
+      },
+      exclude: /node_modules/
+    }]
+  }
+};
+
